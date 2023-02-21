0dbcff2166ce15c031f957f62a6b52937538f6d3
rsmichelle <rs.michellej@gmail.com>
2023-02-21 15:10:11 -0700

    move existing project

---
+++ src/index.js
@@ -0,0 +1,37 @@
+let date = new Date();
+let h2 = document.querySelector("h2");
+let hours = date.getHours();
+if (hours < 10) {
+  hours = `0${hours}`;
+}
+let minutes = date.getMinutes();
+if (minutes < 10) {
+  minutes = `0${minutes}`;
+}
+let dayIndex = date.getDay();
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+let day = days[dayIndex];
+h2.innerHTML = `${day}, ${hours}:${minutes}`;
+
+function search(event) {
+  event.preventDefault();
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let searchCity = document.querySelector("#searchInput").value;
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(showTemp);
+}
+function showTemp(response) {
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#temperature").innerHTML = response.data.main.temp;
+}
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", search);
